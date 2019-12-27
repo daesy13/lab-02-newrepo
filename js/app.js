8383efd3dea78a300f0850ea0ef17d6b5179829f
daesy <daesyramos@hotmail.com>
2019-12-27 02:22:49 -0800

    add switch and filter by animal

--- js/app.js
+++ js/app.js
@@ -8,77 +8,110 @@ function Creatures(image_url, title, description, keyword, horns){
   this.horns=horns;
 }
 
+// Creatures.prototype.renderingWithJQuery = function(){
+//   $('#photo-template').append(`
+//     <div class="${this.keyword}">
+//       <h2>${this.title}</h2>
+//       <img src="${this.image_url}"></img>
+//       <p>${this.description}</p>
+//     </div>`
+//   )
+// }
+
 Creatures.prototype.renderingWithJQuery = function(){
-  $('#photo-template').append(`
-    <div class="${this.keyword}">
-      <h2>${this.title}</h2>
-      <img src="${this.image_url}"></img>
-      <p>${this.description}</p>
-    </div>`
-  )
-}
-
-Creatures.prototype.renderingWithHandlebars = function(){
-
   let hornsTemplate = $('#horns-template').html();
-
   let hbHornsTemplate = Handlebars.compile(hornsTemplate);
-
   let html = hbHornsTemplate(this);
-  console.log('Creatures Object', this);
-
   $('#photo-template').append(html);
 }
 
-$.get('data/page-1.json').then(
-  (data) => {
-    data.forEach(creatureObjFromFile => {
-      let creature = new Creatures( creatureObjFromFile.image_url,creatureObjFromFile.title, creatureObjFromFile.description, creatureObjFromFile.keyword, creatureObjFromFile.horns);
-      // creature.renderingWithJQuery();
-      creature.renderingWithHandlebars();
-    })
-  }
-)
+// RENDERING BY PAGE
+function getPage(page){
+  $.get(page).then(
+    (data) => {
+      data.forEach(creatureObjFromFile => {
+        let creature = new Creatures( creatureObjFromFile.image_url,creatureObjFromFile.title, creatureObjFromFile.description, creatureObjFromFile.keyword, creatureObjFromFile.horns);
+        creature.renderingWithJQuery();
+      })
+    }
+  )
+}
 
+// CALLING RENDERING FUNCTION FOR PAGE 1
+getPage('data/page-1.json');
+
+// FILTER BY TYPE OF CREATURE
 $(document).ready(function(){
   $('#but_read').click(function() {
     let $animal = $('#keyword-dropdown option:selected').text();
-
-    $('#result').html('animal: ' + $animal)
-
-    $('div').hide();
+    $('div[class]').hide();
     $(`div[class="${$animal}"]`).show();
     console.log(`div[class="${$animal}"]`);
   });
 });
 
 
-// PAGE 2 EVENT LISTENER
+// SWITCH PAGES
+let actualPage = 'data/page-1.json';
 
 $(document).ready(function(){
-  $('#page_button').click(function() {
+  $('#page_button').click(function(){
     $('div').hide();
-    $.get('data/page-2.json').then(
-      (data) => {
-        data.forEach(creatureObjFromFile => {
-          let creature = new Creatures( creatureObjFromFile.image_url,creatureObjFromFile.title, creatureObjFromFile.description, creatureObjFromFile.keyword, creatureObjFromFile.horns);
-          creature.renderingWithJQuery();
-        })
+    if(actualPage === 'data/page-1.json'){
+      const divs = $('div[data-page="data/page-2.json"]');
+      if(!divs.length){
+        getPage('data/page-2.json');
       }
-    )
+      divs.show();
+      actualPage = 'data/page-2.json';
+    } 
+    else {
+      const divs = $('div[data-page="data/page-1.json"]');
+      if (!divs.length) {
+        getPage('data/page-1.json');
+      }
+      divs.show();
+      actualPage = 'data/page-1.json';
+    }
   });
 });
 
-// SORT BY HORN 
+// DROPDOWN SELECTION
 // $(document).ready(function(){
-//   $('#but_filter').click(function() {
+//   // $("#keyword-dropdown").select2();
+//   $('#but_read').click(function() {
 //     let $animal = $('#keyword-dropdown option:selected').text();
 
 //     $('#result').html('animal: ' + $animal)
+//     // $('div').each(function(value){
 
+//     // })
 //     $('div').hide();
+//     // $('div class=this.keyword').show();
 //     $(`div[class="${$animal}"]`).show();
 //     console.log(`div[class="${$animal}"]`);
+//     if ($(this).keyword !== animal)
+//       console.log('animal:', animal)
+//       $(this).hide();
+
+//     if ($(this).keyword === animal)
+//       $(this).show();
 //   });
 // });
-// this is a test
+
+// // SWITCH PAGE ON PAGE 2
+// // $(document).ready(function(){
+// //   $('#page_button').click(function() {
+// //     $('div').hide();
+// //     $.get('data/page-1.json').then(
+// //       (data) => {
+// //         data.forEach(creatureObjFromFile => {
+// //           let creature = new Creatures( creatureObjFromFile.image_url,creatureObjFromFile.title, creatureObjFromFile.description, creatureObjFromFile.keyword, creatureObjFromFile.horns);
+// //           creature.renderingWithJQuery();
+// //         })
+// //       }
+// //     )
+// //   });
+// // });
+
+
